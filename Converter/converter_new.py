3527a5fa727225d7c411ae50522d34c172fa46eb
Nejc Pivec <nejc.pivec1994@gmail.com>
2019-01-17 20:42:31 +0100

    initial commit

---
+++ Converter/converter_new.py
@@ -0,0 +1,28 @@
+import time
+
+print("Pozdravljen, sem program, ki pretvarja kilometre v milje!")
+
+while True:
+    try:
+        print("Vnesi število kilometrov, ki jih želiš pretvoriti: ")
+        vnos = int(input())
+    except ValueError:
+        print("POZOR vnesti poizkušate črko. Vnesi številsko vrednost!")
+        time.sleep(2) #vstavimo nek delay
+        continue
+    except:
+        print("Nekaj se je zalomilo, napako bomo popravili v najkrajšem možnem času.")
+
+    milje = vnos * 0.621371192
+    milje = str(round(milje, 2))
+
+    print("Vaše število se pretvori v " + milje + " milje")
+
+    nadaljevanje = input("Želiš nadaljevati: [y/n]")
+
+    if nadaljevanje.lower() == "n":
+        print("Hvala za vašo uporabo in lep pozdrav!")
+        break
+
+
+
